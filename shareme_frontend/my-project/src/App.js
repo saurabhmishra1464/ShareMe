4b7e2b920042420e64f0df773823af0ad5ad9330
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-05-20 12:52:14 +0530

    sidebar changes

--- shareme_frontend/my-project/src/App.js
+++ shareme_frontend/my-project/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <Routes>
             <Route path="login" element={<Login />} />
-            <Route path='home' element={<Home />} />
+            <Route path='/*' element={<Home />} />
         </Routes>
     )
 }
