fef4ecf236c7d912624e4b229fe8f5d749f66031
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-05-23 15:35:48 +0530

    create-pin with upload image

--- shareme_frontend/my-project/src/components/Feed.jsx
+++ shareme_frontend/my-project/src/components/Feed.jsx
@@ -1,33 +1,33 @@
-import React, {useState,useEffect} from 'react' ;
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import {client} from '../client';
+import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 import { feedQuery, searchQuery } from '../Utils/data';
-const Feed = () => {  
+const Feed = () => {
   const [loading, setLoading] = useState(false);
   const [pins, setPins] = useState(null);
-  const {categoryId} = useParams();
-  useEffect(()=>{
+  const { categoryId } = useParams();
+  useEffect(() => {
     setLoading(true);
-if(categoryId){
-const query = searchQuery(categoryId);
-client.fetch(query)
-.then((data)=> {
-  setPins(data);
-  setLoading(false);
-})
-}else{
-client.fetch(feedQuery).then((data)=>{
-  setPins(data);
-  setLoading(false);
-})
-}
-  },[categoryId]) //recall the useeffect every time the category changes.
-  if(loading) return <Spinner message="We are adding new ideas to your feed!"/>
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  }, [categoryId]) //recall the useeffect every time the category changes.
+  if (loading) return <Spinner message="We are adding new ideas to your feed!" />
   return (
-    <div>{pins && <MasonryLayout pins={pins}/>}</div>
+    <div>{pins && <MasonryLayout pins={pins} />}</div>
   )
 }
 
